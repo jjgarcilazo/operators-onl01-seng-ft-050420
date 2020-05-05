5a9be116d480c915a7a4f59d24803cdac628a85e
Jesse Garcilazo <jjgarcilazo@gmail.com>
2020-05-05 22:26:09 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,27 +1,10 @@
 def unsafe?(speed)
-
-def unsafe?(speed)
   if speed < 40 || speed > 60
     return true
   else
     false
   end
 end
-
-
-
-
-=begin
-def unsafe?(speed)
-  if speed > 60
-    true
-  elsif speed < 40
-    true
-  else
-    false
-  end
-end
-=end
 def not_safe?(speed)
 
 	speed < 40 || speed > 60 ? true : false
